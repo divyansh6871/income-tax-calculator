6c026ce5049f7e1532654efd1fc8ab6bc7257804
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-11-20 07:30:06 +0530

    Update old.js

--- script/old.js
+++ script/old.js
@@ -8,22 +8,26 @@ getval = () => {
     var mediclaim = Number.parseInt(document.getElementById("input7").value);
     var child_tution_fee = Number.parseInt(document.getElementById("input8").value);
     var standard_deduction = 50000
-    var taxable=gross-hra-lta+house+other-pf-mediclaim-child_tution_fee-standard_deduction
-    console.log(taxable)
-    if (taxable < 250000) {
-        const tax = 0
+    var taxable1=gross-hra-lta+house+other-pf-mediclaim-child_tution_fee-standard_deduction
+    console.log(taxable1)
+    if (taxable1 < 250000) {
+        var oldtax = 0
     }
-    else if (taxable >= 250000 && taxable < 500000) {
-        const tax = (0.05 * taxable-250000)
+    else if (taxable1 >= 250000 && taxable1 < 500000) {
+        var oldtax = (0.05 * taxable1-250000)
     }
-    else if (taxable >= 500000 && taxable < 1000000) {
-        var tax = (0.05 * 250000 + 0.2 * (taxable - 500000))
+    else if (taxable1 >= 500000 && taxable1 < 1000000) {
+        var oldtax = (0.05 * 250000 + 0.2 * (taxable1 - 500000))
     }
-    else (taxable >= 1000000 && taxable < 1500000) 
-        var tax = (0.05 * 250000 + 0.2 * 500000 + 0.3 * (taxable - 1000000))
-    console.log(tax)
+    else (taxable1 >= 1000000 && taxable1 < 1500000) 
+        var oldtax = (0.05 * 250000 + 0.2 * 500000 + 0.3 * (taxable1 - 1000000))
+    console.log(oldtax)
+    if(oldtax>0){
     var taxable_box = document.getElementById("taxable_income")
-    taxable_box.innerHTML = taxable
+    taxable_box.innerHTML = taxable1
     var tax_box = document.getElementById("total_tax")
-    tax_box.innerHTML = tax
+    tax_box.innerHTML = oldtax}    
+    else {
+        alert("You might have not filled all the inputs\nPlease do enter all the fileds")
+    }
 }
