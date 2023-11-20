0d35da8c22cf280132f6494498aaf8234db64897
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-11-20 07:29:42 +0530

    Update new.js

--- script/new.js
+++ script/new.js
@@ -3,31 +3,35 @@ getval = () => {
     var house = Number.parseInt(document.getElementById("input2").value);
     var other = Number.parseInt(document.getElementById("input3").value);
     var standard_deduction = 50000
-    var taxable=ctc+house+other-standard_deduction
-    console.log(taxable)
-    if (taxable < 250000) {
+    var taxable2=ctc+house+other-standard_deduction
+    console.log(taxable2)
+    if (taxable2 < 250000) {
         const tax = 0
     }
-    else if (taxable >= 250000 && taxable < 500000) {
-        const tax = (0.05 * taxable)
+    else if (taxable2 >= 250000 && taxable2 < 500000) {
+        var newtax = (0.05 * taxable2)
     }
-    else if (taxable >= 500000 && taxable < 750000) {
-        var tax = (0.05 * 250000 + 0.1 * (taxable - 500000))
+    else if (taxable2 >= 500000 && taxable2 < 750000) {
+        var newtax = (0.05 * 250000 + 0.1 * (taxable2 - 500000))
     }
-    else if (taxable >= 750000 && taxable < 1000000) {
-        var tax = (0.05 * 250000 + 0.1 * 250000 + .15 * (taxable - 750000))
+    else if (taxable2 >= 750000 && taxable2 < 1000000) {
+        var newtax = (0.05 * 250000 + 0.1 * 250000 + .15 * (taxable2 - 750000))
     }
-    else if (taxable >= 1000000 && taxable < 1250000) {
-        var tax = (0.05 * 250000 + 0.1 * 250000 + .15 * 250000 + 0.2 * (taxable - 1000000))
+    else if (taxable2 >= 1000000 && taxable2 < 1250000) {
+        var newtax = (0.05 * 250000 + 0.1 * 250000 + .15 * 250000 + 0.2 * (taxable2 - 1000000))
     }
-    else if (taxable >= 1250000 && taxable < 1500000) {
-        var tax = (0.05 * 250000 + 0.1 * 250000 + .15 * 250000 + 0.2 * 250000 + 0.25 * (taxable - 1250000))
+    else if (taxable2 >= 1250000 && taxable2 < 1500000) {
+        var newtax = (0.05 * 250000 + 0.1 * 250000 + .15 * 250000 + 0.2 * 250000 + 0.25 * (taxable2 - 1250000))
     }
-    else (taxable > 1500000) 
-        var tax = (0.05 * 250000 + 0.1 * 250000 + .15 * 250000 + 0.2 * 250000 + 0.25 * 250000 + 0.3 * (taxable - 1500000))
-    console.log(tax)
+    else (taxable2 > 1500000) 
+        var newtax = (0.05 * 250000 + 0.1 * 250000 + .15 * 250000 + 0.2 * 250000 + 0.25 * 250000 + 0.3 * (taxable2 - 1500000))
+    console.log(newtax)
+    if(newtax>0){
     var taxable_box=document.getElementById("taxable_income")
-    taxable_box.innerHTML=taxable
+    taxable_box.innerHTML=taxable2
     var tax_box=document.getElementById("total_tax")
-    tax_box.innerHTML=tax
+    tax_box.innerHTML=newtax
+    }
+    else
+    alert("You might have not filled all the inputs")
 }
