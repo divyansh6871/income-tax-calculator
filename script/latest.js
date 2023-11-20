6820ce4ad7c1e93f308fe226105a792acc40b9ff
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-11-20 07:29:11 +0530

    Update latest.js

--- script/latest.js
+++ script/latest.js
@@ -3,28 +3,34 @@ getval = () => {
     var house = Number.parseInt(document.getElementById("input2").value);
     var other = Number.parseInt(document.getElementById("input3").value);
     var standard_deduction = 50000
-    var taxable=ctc+house+other-standard_deduction
-    console.log(taxable)
-    if (taxable < 300000) {
-        const tax = 0
+    var taxable3 = ctc + house + other - standard_deduction
+    console.log(taxable3)
+    if (taxable3 < 300000) {
+        var latesttax = 0
     }
-    else if (taxable >= 300000 && taxable < 600000) {
-        const tax = (0.05 * taxable-300000)
+    else if (taxable3 >= 300000 && taxable3 < 600000) {
+        var latesttax = (0.05 * taxable3 - 300000)
     }
-    else if (taxable >= 600000 && taxable < 900000) {
-        var tax = (0.05 * 300000 + 0.1 * (taxable - 600000))
+    else if (taxable3 >= 600000 && taxable3 < 900000) {
+        var latesttax = (0.05 * 300000 + 0.1 * (taxable3 - 600000))
     }
-    else if (taxable >= 900000 && taxable < 1200000) {
-        var tax = (0.05 * 300000 + 0.1 * 300000 + .15 * (taxable - 900000))
+    else if (taxable3 >= 900000 && taxable3 < 1200000) {
+        var latesttax = (0.05 * 300000 + 0.1 * 300000 + .15 * (taxable3 - 900000))
     }
-    else if (taxable >= 1200000 && taxable < 1500000) {
-        var tax = (0.05 * 300000 + 0.1 * 300000 + .15 * 300000 + 0.2 * (taxable - 1200000))
+    else if (taxable3 >= 1200000 && taxable3 < 1500000) {
+        var latesttax = (0.05 * 300000 + 0.1 * 300000 + .15 * 300000 + 0.2 * (taxable3 - 1200000))
     }
-    else
-        var tax = (0.05 * 300000 + 0.1 * 300000 + .15 * 300000 + 0.2 * 300000 + 0.3 * 300000 + 0.3 * (taxable - 1500000))
-    console.log(tax)
-    var taxable_box=document.getElementById("taxable_income")
-    taxable_box.innerHTML=taxable
-    var tax_box=document.getElementById("total_tax")
-    tax_box.innerHTML=tax
+    else {
+        var latesttax = (0.05 * 300000 + 0.1 * 300000 + .15 * 300000 + 0.2 * 300000 + 0.3 * 300000 + 0.3 * (taxable3 - 1500000))
+    }
+    if (latesttax > 0) {
+        console.log(latesttax)
+        var taxable_box = document.getElementById("taxable_income")
+        taxable_box.innerHTML = taxable3
+        var tax_box = document.getElementById("total_tax")
+        tax_box.innerHTML = latesttax
+    }
+    else {
+        alert("You might have not filled all the inputs")
+    }
 }
