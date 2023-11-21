7e7cc0499a58aed77d49eebdae21e6ca2aef5e34
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-11-21 19:44:02 +0530

    Update new.js

--- script/new.js
+++ script/new.js
@@ -6,7 +6,7 @@ getval = () => {
     var taxable2=ctc+house+other-standard_deduction
     console.log(taxable2)
     if (taxable2 < 250000) {
-        const tax = 0
+        var newtax = 0
     }
     else if (taxable2 >= 250000 && taxable2 < 500000) {
         var newtax = (0.05 * taxable2)
@@ -23,14 +23,14 @@ getval = () => {
     else if (taxable2 >= 1250000 && taxable2 < 1500000) {
         var newtax = (0.05 * 250000 + 0.1 * 250000 + .15 * 250000 + 0.2 * 250000 + 0.25 * (taxable2 - 1250000))
     }
-    else (taxable2 > 1500000) 
+    else
         var newtax = (0.05 * 250000 + 0.1 * 250000 + .15 * 250000 + 0.2 * 250000 + 0.25 * 250000 + 0.3 * (taxable2 - 1500000))
     console.log(newtax)
-    if(newtax>0){
-    var taxable_box=document.getElementById("taxable_income")
-    taxable_box.innerHTML=taxable2
-    var tax_box=document.getElementById("total_tax")
-    tax_box.innerHTML=newtax
+    if(taxable2>0){
+        var tablebox = document.getElementById("conclusion")
+        tablebox.innerHTML = `<button class="btn" style="background-color: rgb(214, 139, 0) ;color: white;font-weight: 400;">Your taxable income is : ${taxable2}.</button><br><br>
+    <button class="btn" style="background-color: rgb(214, 139, 0) ;color: white;font-weight: 400;">Tax to be paid is ${newtax}.</button>
+  `
     }
     else
     alert("You might have not filled all the inputs")
