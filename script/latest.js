6449887a014ea4cd77933810945ece6f8ec0f3fc
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-11-21 19:43:50 +0530

    Update latest.js

--- script/latest.js
+++ script/latest.js
@@ -9,26 +9,26 @@ getval = () => {
         var latesttax = 0
     }
     else if (taxable3 >= 300000 && taxable3 < 600000) {
-        var latesttax = (0.05 * taxable3 - 300000)
+        var latesttax = 0.05 * (taxable3 - 300000)
     }
     else if (taxable3 >= 600000 && taxable3 < 900000) {
-        var latesttax = (0.05 * 300000 + 0.1 * (taxable3 - 600000))
+        var latesttax = (0.05 * 300000) + 0.1 * (taxable3 - 600000)
     }
     else if (taxable3 >= 900000 && taxable3 < 1200000) {
-        var latesttax = (0.05 * 300000 + 0.1 * 300000 + .15 * (taxable3 - 900000))
+        var latesttax = (0.05 * 300000) + 0.1 * 300000 + .15 * (taxable3 - 900000)
     }
     else if (taxable3 >= 1200000 && taxable3 < 1500000) {
-        var latesttax = (0.05 * 300000 + 0.1 * 300000 + .15 * 300000 + 0.2 * (taxable3 - 1200000))
+        var latesttax = 0.05 * 300000 + 0.1 * 300000 + .15 * 300000 + 0.2 * (taxable3 - 1200000)
     }
     else {
-        var latesttax = (0.05 * 300000 + 0.1 * 300000 + .15 * 300000 + 0.2 * 300000 + 0.3 * 300000 + 0.3 * (taxable3 - 1500000))
+        var latesttax = 0.05 * 300000 + 0.1 * 300000 + .15 * 300000 + 0.2 * 300000 + 0.3 * 300000 + 0.3 * (taxable3 - 1500000)
     }
-    if (latesttax > 0) {
+    if (taxable3 > 0) {
         console.log(latesttax)
-        var taxable_box = document.getElementById("taxable_income")
-        taxable_box.innerHTML = taxable3
-        var tax_box = document.getElementById("total_tax")
-        tax_box.innerHTML = latesttax
+        var tablebox = document.getElementById("conclusion")
+        tablebox.innerHTML = `<button class="btn" style="background-color: rgb(214, 139, 0) ;color: white;font-weight: 400;">Your taxable income is : ${taxable3}.</button><br><br>
+    <button class="btn" style="background-color: rgb(214, 139, 0) ;color: white;font-weight: 400;">Tax to be paid is ${latesttax}.</button>
+  `
     }
     else {
         alert("You might have not filled all the inputs")
